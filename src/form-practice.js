dc11206b31cd80f5a0f6fa26a28e5a39c09339f8
JonD45 <jonathan.dohrman@student.swic.edu>
2025-11-20 15:44:16 -0800

    feat try its 1-3

---
+++ src/form-practice.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("form");
+
+// Same pattern as 'clicks', but for 'submit' events.
+form.addEventListener("submit", (event) => {
+  // No, browser! WE will handle this. Stop your default behavior.
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+});
+
+function Results(data) {
+  console.log(Object.entries(data));
+
+  return `<ul class="space-y-1 text-sm">
+        ${Object.entries(data)
+          .map(([k, v]) => `<li><strong>${k}:</strong> ${v}</li>`)
+          .join("")}
+    </ul>   
+    `;
+}
